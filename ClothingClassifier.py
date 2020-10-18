83ab7ae7cb2830433a71d852193eaf48f441d30b
Eddy Zhou <edward.zhzh@gmail.com>
2020-10-17 21:38:22 -0400

    Using Fashion MNIST

---
+++ ClothingClassifier.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# This is the image preprocessed, the pixel values
+# fall in a range of 255 and must be normalized
+"""plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()"""
+
+# Normalization of Data (Pixel Values)
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap='binary')
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
