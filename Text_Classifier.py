533d067df63e90302786a5aeacb0fa5d9dc1ee76
Eddy Zhou <edward.zhzh@gmail.com>
2020-10-18 17:16:08 -0400

    text classifier

--- Text_Classifier.py
+++ Text_Classifier.py
@@ -29,14 +29,88 @@ seed = 42
 
 raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
     'aclImdb/train',
-    batch_size = batchSize,
-    validation_split = 0.2, # 20% of the training set will be a validation set
+    batch_size=batchSize,
+    validation_split=0.2,
+    # 80% of the training set will be a training set
     subset='training',
     seed=seed)
 
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batchSize,
+    validation_split=0.2,
+    # 20% of the training set will be a validation set
+    subset='validation',
+    seed=seed)
 
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batchSize)
 
+# Now that the data has been divided, it must now be standardized, tokenized, and vectorized
+# Standardize: remove punctuations or HTML
+# Tokenize: splitting strings into tokens, eg sentences to its words
+# Vectorization: converts these tokens into numbers so they can be fed into a neural network
 
+def custom_standardization(input_data): # is needed because default standardizer cannot remove html
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html,
+                                    '[%s]' % re.escape(string.punctuation),
+                                    '')
 
+max_features = 10000
+sequence_length = 250
 
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
 
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+# Now that the data has be preprocessed, it needs to run in a efficient way as to not clog memory
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Time to create the model!
+embedding_dim = 16
+
+model = tf.keras.Sequential([
+    layers.Embedding(max_features + 1, embedding_dim),
+    layers.Dropout(0.2),
+    layers.GlobalAveragePooling1D(),
+    layers.Dropout(0.2),
+    layers.Dense(1)])
+
+model.summary()
+
+model.compile(loss=losses.BinaryCrossentropy(from_logits=True),
+              optimizer='adam',
+              metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data = val_ds,
+    epochs=epochs)
+
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+# Analyze the accuracy and loss overtime
