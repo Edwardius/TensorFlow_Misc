71a28b79054632c4c583c8079910feccb7e3e1e6
Eddy Zhou <edward.zhzh@gmail.com>
2020-10-20 16:56:52 -0400

    Stack Classifier

---
+++ Stack_Overflow_Classifier.py
@@ -0,0 +1,110 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import preprocessing
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+url = "http://storage.googleapis.com/download.tensorflow.org/data/stack_overflow_16k.tar.gz"
+
+dataSet = tf.keras.utils.get_file("stack_overflow_16k.tar.gz", url,
+                                  untar=True, cache_dir='.',
+                                  cache_subdir='')
+
+# No useless files
+
+# Creating Validation Set
+batchSize = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'train',
+    batch_size=batchSize,
+    validation_split=0.2,
+    # 80% of the training set will be a training set
+    subset='training',
+    seed=seed)
+
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'train',
+    batch_size=batchSize,
+    validation_split=0.2,
+    # 20% of the training set will be a validation set
+    subset='validation',
+    seed=seed)
+
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'test',
+    batch_size=batchSize)
+
+# Standardize, Tokenize, Vectorized
+
+
+def custom_standardization(input_data): # just incase if file contains html
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html,
+                                    '[%s]' % re.escape(string.punctuation),
+                                    '')
+
+
+max_features = 5000
+sequence_length = 500
+
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+# Memory Management
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Create Model
+embedding_dim = 128
+
+model = tf.keras.Sequential([
+    layers.Embedding(max_features + 1, embedding_dim),
+    layers.Dropout(0.2),
+    layers.GlobalAveragePooling1D(),
+    layers.Dropout(0.2),
+    layers.Dense(4)])
+
+model.summary()
+
+model.compile(loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+              optimizer='adam',
+              metrics=['accuracy'])
+
+epochs = 25
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
